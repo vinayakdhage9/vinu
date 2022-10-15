17c504414bc81c4f1b16e4399fe52a8e3773fc05
Akashkp7 <a9890499661@gamil.com>
2022-10-15 15:49:54 +0530

    add

--- first.py
+++ first.py
@@ -1,4 +1,12 @@
-a=int(input('enter the first no.'))
-b=int(input('enter the second no.'))
-c=a+b
-print('the sum of {0} and {1} is= {2}'.format(a,b,c)clear
+from operator import le 
+import pandas as pd
+info={'Gender':['Male','female','male','female'],'position':['head','Asst.prof','head','Asst.prof']}
+df=pd.DataFrame(info)
+print(df)
+from sklearn.preprocessing import LabelEncoder
+le=LabelEncoder()
+gender_encoded=le.fit_transform(df['Gender'])
+encoded_position=le.fit_transform(df['Position'])
+df['Encoded_Gender']=gender_encoded
+df['Encoded_Postition']=encoded_position
+print(df)
---
+++ v.py
@@ -0,0 +1,4 @@
+from operator import le
+import pandas as pd
+dataset=pd.read_csv(page.csv)
+print(dataset)
